e049628e2cd02d167a717fac5fb1647d8abd0f85
Aleksander <halekun@gmail.com>
2020-05-26 09:30:56 +0200

    Test setTimeout in unfocused tab

--- script.js
+++ script.js
@@ -9,5 +9,42 @@ let meep = (function()
 	};
 })();
 //-----------------//
+async function sleep(ms)
+{
+	return new Promise(resolve => setTimeout(resolve ,ms));
+}
+//-----------------//
 
-meep("Hello, World!");
+meep("Hello, World!")
+let cont = true
+document.addEventListener('keydown', event => {
+	if (event.ctrlKey && event.code === 'KeyC') {
+		cont = false
+		meep("SIGINT")
+	}
+})
+document.addEventListener('blur', _ => {
+	meep("document BLUR")
+})
+document.addEventListener('focus', _ => {
+	meep("document FOCUS")
+})
+window.addEventListener('blur', _ => {
+	meep("window BLUR")
+})
+window.addEventListener('focus', _ => {
+	meep("window FOCUS")
+})
+
+fun()
+
+async function fun()
+{
+	while(cont)
+	{
+		let t0 = Date.now()
+		await sleep(1000)
+		let t1 = Date.now()
+		meep("Δ:", t1-t0)
+	}
+}
